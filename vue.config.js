409de59a0108f948a3a721ac18f6cb0b1a94c939
sergeyprimirenkov <serg_miren@mail.ru>
2023-04-07 19:33:13 +0500

    add files for static (offline) catalog

---
+++ vue.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  devServer: {
+    proxy: 'http://localhost:8080/',
+  },
+  publicPath: 'catalog-offline',
+  pwa: {
+    // workboxPluginMode: "GenerateSW",
+    // workboxOptions: {
+    //   globIgnores: ["assets/images.*.jpg"],
+    //   exclude: [".*\.jpg"],
+    //   swSrc: "./service-worker.js",
+    // },
+    name: "DriV3R.Каталог",
+    themeColor: "#0e0e30",
+    backgroundColor: "#FFFFFF",
+    // favicon16: '/assets/icons/favicon-32x32.png',
+    // favicon32: '/assets/icons/favicon-32x32.png',
+    // appleTouchIcon: '/assets/icons/apple-touch-icon-152x152.png',
+    // msTileImage: '/assets/icons/msapplication-icon-144x144.png',
+    iconPaths: {
+      favicon16: '/assets/icons/favicon-16x16.png',
+      favicon32: '/assets/icons/favicon-32x32.png',
+      appleTouchIcon: '/assets/icons/apple-touch-icon.png',
+      maskIcon: '/assets/icons/android-chrome-192x192.svg',
+      msTileImage: '/assets/icons/android-chrome-512x512.png'
+    },
+    manifestOptions: {
+      icons: [
+        { 'src': '/assets/icons/favicon-16x16.png', 'sizes': '16x16', 'type': 'image/png' },
+        { 'src': '/assets/icons/favicon-16x16.png', 'sizes': '32x32', 'type': 'image/png' },
+        { 'src': '/assets/icons/apple-touch-icon', 'sizes': '180x180', 'type': 'image/png' },
+        { 'src': '/assets/icons/android-chrome-192x192.png', 'sizes': '192x192', 'type': 'image/png' },
+        { 'src': '/assets/icons/android-chrome-512x512.png', 'sizes': '512x512', 'type': 'image/png' },
+      ]
+    }
+  }
+}
