2404c0b522c04abcaebc13213fd900c5c7e42589
sergeyprimirenkov <serg_miren@mail.ru>
2023-04-07 20:20:13 +0500

    fix file paths

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   devServer: {
     proxy: 'http://localhost:8080/',
   },
-  publicPath: 'catalog-offline',
+  publicPath: '',
   pwa: {
     // workboxPluginMode: "GenerateSW",
     // workboxOptions: {
@@ -21,14 +21,14 @@ module.exports = {
       favicon16: '/assets/icons/favicon-16x16.png',
       favicon32: '/assets/icons/favicon-32x32.png',
       appleTouchIcon: '/assets/icons/apple-touch-icon.png',
-      maskIcon: '/assets/icons/android-chrome-192x192.svg',
+      maskIcon: '/assets/icons/android-chrome-192x192.png',
       msTileImage: '/assets/icons/android-chrome-512x512.png'
     },
     manifestOptions: {
       icons: [
         { 'src': '/assets/icons/favicon-16x16.png', 'sizes': '16x16', 'type': 'image/png' },
         { 'src': '/assets/icons/favicon-16x16.png', 'sizes': '32x32', 'type': 'image/png' },
-        { 'src': '/assets/icons/apple-touch-icon', 'sizes': '180x180', 'type': 'image/png' },
+        { 'src': '/assets/icons/apple-touch-icon.png', 'sizes': '180x180', 'type': 'image/png' },
         { 'src': '/assets/icons/android-chrome-192x192.png', 'sizes': '192x192', 'type': 'image/png' },
         { 'src': '/assets/icons/android-chrome-512x512.png', 'sizes': '512x512', 'type': 'image/png' },
       ]
